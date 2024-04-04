4c923ed0eabe5dc0162a63ff8ff687db54b13bc3
Tayo Akosile <oluwatayoakosile@gmail.com>
2024-04-04 07:25:31 -0700

    feat:Updated Default Content on Page

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,7 +1,24 @@
 <template>
-<section></section>  
+  <section>
+    <div class="frame"></div>
+  </section>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.frame {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  height: 400px;
+  margin-top: -200px;
+  margin-left: -200px;
+  border-radius: 2px;
+  box-shadow: 1px 2px 10px 0px rgba(0, 0, 0, 0.3);
+  background: #46a6da;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
